5db469a5c08713d5fccdc9e7ec69c36dd4be1146
Shekhar Ullah <ullah.shekhar@gmail.com>
2017-09-18 13:49:30 +0600

    Hello world project with beans.xml

---
+++ projects/sts/01_HelloWorld/src/main/java/com/corespring/learning/_HelloWorld/App.java
@@ -0,0 +1,19 @@
+package com.corespring.learning._HelloWorld;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ApplicationContext context= new FileSystemXmlApplicationContext("beans.xml");
+        Patient patient =(Patient)context.getBean("patient");
+        patient.Speak();
+        ((FileSystemXmlApplicationContext)context).close();
+    }
+}
---
+++ projects/sts/01_HelloWorld/src/main/java/com/corespring/learning/_HelloWorld/Patient.java
@@ -0,0 +1,9 @@
+package com.corespring.learning._HelloWorld;
+
+public class Patient {
+
+	public void Speak() {
+		System.out.println("Help Me!");
+	}
+
+}
